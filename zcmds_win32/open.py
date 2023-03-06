e94de5123e0d5223f8e69be91451b78806e10816
zackees <z@zackees.com>
2023-03-06 00:08:11 -0800

    bump 1.0.8

--- zcmds_win32/open.py
+++ zcmds_win32/open.py
@@ -1,10 +1,11 @@
 import os
 import sys
+from zcmds_win32._exec import os_exec
 
 
 def main() -> int:
     cmd = "explorer"
     if len(sys.argv) == 1:
         cmd += " ."
-    print(cmd)
-    return os.system(cmd)
+        return os.system(cmd)
+    return os_exec(cmd)
