8655e00c61013426cad95cac70a342315f188656
zackees <z@zackees.com>
2023-03-14 20:32:10 -0700

    fixvmmem

--- zcmds_win32/elevated_exec.py
+++ zcmds_win32/elevated_exec.py
@@ -9,6 +9,7 @@ Executes an elevated command in windows. Very tricky.
 import atexit
 import os
 import shutil
+from tempfile import TemporaryDirectory
 import time
 
 
@@ -21,43 +22,45 @@ def write_utf8(path: str, content: str) -> None:
 def elevated_exec(cmd: str) -> None:
     """Execute a command as admin."""
 
-    def get_path(file: str) -> str:
-        os.makedirs(".tmp", exist_ok=True)
-        path = os.path.join(".tmp", file)
-        out = os.path.abspath(path)
-        return out
+    with TemporaryDirectory(ignore_cleanup_errors=True) as tmpdir:
 
-    write_bat_file = get_path("write.bat")
-    run_bat_file = get_path("myrun.bat")
-    ps1_file = get_path("run.ps1")
-    done_txt = get_path("done.txt")
+        def get_path(file: str) -> str:
+            os.makedirs(tmpdir, exist_ok=True)
+            path = os.path.join(tmpdir, file)
+            out = os.path.abspath(path)
+            return out
 
-    atexit.register(lambda: shutil.rmtree(".tmp", True))
+        write_bat_file = get_path("write.bat")
+        run_bat_file = get_path("myrun.bat")
+        ps1_file = get_path("run.ps1")
+        done_txt = get_path("done.txt")
 
-    write_cmd = f"""
-    @echo off
-    echo done > "{done_txt}"
-    """
+        atexit.register(lambda: shutil.rmtree(".tmp", True))
 
-    # First execute the service as admin.
-    # Then execute write.bat as a normal user, "done.txt" will appear.
-    admin_cmd = f"""
-    @echo off
-    {cmd}
-    runas /trustlevel:0x20000 "{write_bat_file}"
-    """
+        write_cmd = f"""
+        @echo off
+        echo done > "{done_txt}"
+        """
 
-    entrypoint_ps1 = f"""
-    Start-Process -Verb runAs "{run_bat_file}"
-    """
+        # First execute the service as admin.
+        # Then execute write.bat as a normal user, "done.txt" will appear.
+        admin_cmd = f"""
+        @echo off
+        {cmd}
+        runas /trustlevel:0x20000 "{write_bat_file}"
+        """
 
-    write_utf8(write_bat_file, write_cmd)
-    write_utf8(run_bat_file, admin_cmd)
-    write_utf8(ps1_file, entrypoint_ps1)
+        entrypoint_ps1 = f"""
+        Start-Process -Verb runAs "{run_bat_file}"
+        """
 
-    cmd = f'powershell -c "{os.path.abspath(ps1_file)}"'
-    # print(cmd)
-    os.system(cmd)
-    # time.sleep(20)
-    while not os.path.exists(write_bat_file):
-        time.sleep(0.1)
+        write_utf8(write_bat_file, write_cmd)
+        write_utf8(run_bat_file, admin_cmd)
+        write_utf8(ps1_file, entrypoint_ps1)
+
+        cmd = f'powershell -c "{os.path.abspath(ps1_file)}"'
+        # print(cmd)
+        os.system(cmd)
+        # time.sleep(20)
+        while not os.path.exists(write_bat_file):
+            time.sleep(0.1)
