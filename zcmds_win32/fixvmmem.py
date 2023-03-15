c71c6ae08ca068e2ea56f40e9d060c8b731fb4af
zackees <z@zackees.com>
2023-03-14 19:02:36 -0700

    fix fixvmmem

--- zcmds_win32/fixvmmem.py
+++ zcmds_win32/fixvmmem.py
@@ -1,11 +1,18 @@
 import os
-from subprocess import check_output
+import atexit
+import time
 
+CMD = 'taskkill /F /im wslservice.exe'
+BAT_FILE = os.path.abspath("fixmme.bat")
 
 def main() -> int:
-    current_user = check_output("whoami", shell=True).decode("utf-8").strip()
-    cmd = f'runas /user:"{current_user}" "taskkill /F /im wslservice.exe"'
-    return os.system(cmd)
+    with open(BAT_FILE, encoding="utf-8", mode="w") as f:
+        f.write(CMD)
+    atexit.register(os.remove, BAT_FILE)
+    cmd = f"""powershell -c "Start-Process Powershell -Verb runAs '{BAT_FILE}'" """
+    print(cmd)
+    os.system(cmd)
+    time.sleep(20)
 
 
 if __name__ == "__main__":
