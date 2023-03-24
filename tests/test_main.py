0b65abff9dc37580ac54173a4da9954979e605b1
zackees <z@zackees.com>
2023-03-23 19:29:49 -0700

    move commands to it's own directory

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,16 +4,16 @@ import unittest
 class MainTester(unittest.TestCase):
     def test_imports(self) -> None:
         """Tests the imports."""
-        import zcmds_win32.fixvmmem
-        import zcmds_win32.open
+        import zcmds_win32.cmds.fixvmmem
+        import zcmds_win32.cmds.open
         import zcmds_win32.unix_tool_path
         import zcmds_win32._exec
 
     def test_ls(self) -> None:
         """Tests the ls command."""
-        import zcmds_win32.ls
+        import zcmds_win32.cmds.ls
 
-        zcmds_win32.ls.main()
+        zcmds_win32.cmds.ls.main()
 
 
 if __name__ == "__main__":
